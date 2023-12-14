dff80dc1bebab2774f96e97f13699ffd45dd2c51
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-14 19:12:43 +0000

    update variable

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts an array of integers in ascending order,
+ * bubble_sort - Sorts an array of integers in ascending order,
  *               using the Bubble sort algorithm.
  * @array: The array to be sorted.
  * @size: The size of the array.
@@ -10,16 +10,16 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t idx, jdx;
+	size_t step, jdx;
 	int temp, flag;
 
 	if (!array || size < 2)
 		return;
 
-	for (idx = 0; idx < size - 1; idx++)
+	for (step = 0; step < size - 1; step++)
 	{
 		flag = 0; /* swap checker */
-		for (jdx = 0; jdx < size - idx - 1; jdx++)
+		for (jdx = 0; jdx < size - step - 1; jdx++)
 		{
 			if (array[jdx] > array[jdx + 1])
 			{
